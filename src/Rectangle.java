0f36d361ecd3e4515ef6a07fdb03138e0fe66e9c
bache <tarek.bache@ensea.fr>
2021-10-21 09:30:30 +0200

    projetpaintTarekBache

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,18 @@
-public class Rectangle {
+import java.awt.*;
+
+public class Rectangle extends Figure{
+
+    public Rectangle(Color cl, point pt) {
+        super(cl, pt);
+    }
+
+    @Override
+    public void setBoundingBox(int lengthB, int widthB) {
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
 }
